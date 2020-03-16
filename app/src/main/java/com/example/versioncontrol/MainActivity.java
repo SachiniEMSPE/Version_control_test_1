c553fae213cc504baf68d8c7fb76523498e3e902
sachEMSPE <sachini.emspe@gmail.com>
2020-03-15 23:01:07 -0400

    new comment added

--- app/src/main/java/com/example/versioncontrol/MainActivity.java
+++ app/src/main/java/com/example/versioncontrol/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String comment = "new comment";
+    // this is new branch comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
