f9e3e22338445d9f9f234257ee25647ee467531d
sachEMSPE <sachini.emspe@gmail.com>
2020-03-15 23:07:26 -0400

    new number added.

--- app/src/main/java/com/example/versioncontrol/MainActivity.java
+++ app/src/main/java/com/example/versioncontrol/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String comment = "new comment";
+    int newnumber = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
