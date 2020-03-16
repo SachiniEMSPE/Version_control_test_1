5d1490f493f75e2db50dcf8a532a9cc97833cc79
sachEMSPE <sachini.emspe@gmail.com>
2020-03-15 22:53:27 -0400

    comment added.

--- app/src/main/java/com/example/versioncontrol/MainActivity.java
+++ app/src/main/java/com/example/versioncontrol/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String comment = "new comment";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
